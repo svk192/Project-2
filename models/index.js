772d134a1b6d752817ded91f8daf7bb4359e7850
Neha Gupta <er.nehaa.gupta@gmail.com>
2019-09-10 20:20:27 -0400

    login.html

--- models/index.js
+++ models/index.js
@@ -4,7 +4,8 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
-var env = process.env.NODE_ENV || "development";
+//switch to production when finalized
+var env = process.env.NODE_ENV || "test";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
