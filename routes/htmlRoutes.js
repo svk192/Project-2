9701375b7b0a06e66ff38727775eb7a8c77ce1b7
Neha Gupta <er.nehaa.gupta@gmail.com>
2019-09-12 19:24:51 -0400

    Sequelize setup working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,12 +9,18 @@ var path = require("path");
 // Routes
 // =============================================================
 module.exports = function(app) {
-
   // Each of the below routes just handles the HTML page that the user gets sent to.
+  //signup page
+  // app.get("/", function(req, res) {
+  //   //res.send("Welcome to Passport with Sequelize");
+  //   //res.sendFile(path.join(__dirname, "../SignUp.hbs"));
+  //   res.render("SignUp");
+  // });
 
-  // load login
+  //load login
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    //res.send("Welcome to Passport with Sequelize");
+    res.render("SignIn");
   });
 
   // load homepage
@@ -31,5 +37,4 @@ module.exports = function(app) {
   app.get("/rate", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/submit.html"));
   });
-
 };
