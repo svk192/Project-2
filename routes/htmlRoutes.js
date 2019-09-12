c4709b430b698c32ec4d8439344694b287a25d4e
Emily Brown <emily_brown@rogers.com>
2019-09-12 19:06:42 -0400

    swithced all html to handlebars and created intitial setup of html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,35 +1,44 @@
-// *********************************************************************************
-// html-routes.js - this file offers a set of routes for sending users to the various html pages
-// *********************************************************************************
+var db = require("../models");
 
-// Dependencies
-// =============================================================
-var path = require("path");
-
-// Routes
-// =============================================================
 module.exports = function(app) {
-
-  // Each of the below routes just handles the HTML page that the user gets sent to.
-
-  // load login
+//   Load login page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("index");
   });
+//   app.get("/", function(req, res) {
+//     db.Example.findAll({}).then(function(dbExamples) {
+//       res.render("index", {
+//     //     msg: "Welcome!",
+//     //     examples: dbExamples
+//     //   });
+//     // });
+//   });
 
-  // load homepage
+// load index page
   app.get("/welcome", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/homepage.html"));
+    res.render("index");
   });
 
-  // load rate a book page
+  // Load top rated page
   app.get("/top-rated", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/results.html"));
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.render("results", {
+        example: dbExample
+      });
+    });
   });
 
-  // load rate a book search page
+  // load rate page 
+  app.get("/lookup", function(req, res) {
+    res.render("submit");
+  });
+
   app.get("/rate", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/submit.html"));
-  });
+    
+  })
 
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
 };
