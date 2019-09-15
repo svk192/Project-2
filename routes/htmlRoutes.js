f4ef675247c45f762f2e377caf66a6202f4c0019
Sathish Pittala <48255299+sathishbaburaj@users.noreply.github.com>
2019-09-15 15:31:17 -0400

    added google api functionality to serach the books

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,13 @@
 var db = require("../models");
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+// app.use(bodyParser.json());
+app.use(express.urlencoded());
+var axios = require("axios");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// import axios from 'axios';
 
 module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
@@ -14,16 +23,16 @@ module.exports = function(app) {
     //res.send("Welcome to Passport with Sequelize");
     res.render("SignIn");
   });
-//   app.get("/", function(req, res) {
-//     db.Example.findAll({}).then(function(dbExamples) {
-//       res.render("index", {
-//     //     msg: "Welcome!",
-//     //     examples: dbExamples
-//     //   });
-//     // });
-//   });
+  //   app.get("/", function(req, res) {
+  //     db.Example.findAll({}).then(function(dbExamples) {
+  //       res.render("index", {
+  //     //     msg: "Welcome!",
+  //     //     examples: dbExamples
+  //     //   });
+  //     // });
+  //   });
 
-// load index page
+  // load index page
   app.get("/welcome", function(req, res) {
     res.render("index");
   });
@@ -31,20 +40,52 @@ module.exports = function(app) {
   // Load top rated page
   app.get("/top-rated", function(req, res) {
     // db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("results");
+    res.render("results");
     //     example: dbExample
     //   });
     // });
   });
 
-  // load rate page 
+  // load rate page
   app.get("/lookup", function(req, res) {
     res.render("lookup");
   });
+  //load results page
+  app.post("/results", function(req, res) {
+    // console.log("title:" + req.body.title);
+    // console.log("test");
+    // console.log(req.body.title);
+    var query = req.body.title;
+
+    axios
+      .get("https://www.googleapis.com/books/v1/volumes?q=" + query)
+      .then(function(response) {
+        var books = response.data.items;
+        // for (var i = 0; i < books.length; i++) {
+        // console.log(books[0].volumeInfo.title);
+        var title = books[0].volumeInfo.title;
+        var author = books[0].volumeInfo.authors;
+        var plot = books[0].volumeInfo.description;
+        var image = books[0].volumeInfo.imageLinks.smallThumbnail;
+        // for (var i = 0; i < books.length; i++) {
+        //   console.log("bookTitle:" + books[i].volumeInfo.title);
+        //   console.log("authors:" + books[i].volumeInfo.authors);
+        //   console.log("description:" + books[i].volumeInfo.description);
+        //   console.log("image:" + books[i].volumeInfo.imageLinks.smallThumbnail);
+
+        res.render("results", {
+          bookTitle: title,
+          bookAuthor: author,
+          bookPlot: plot,
+          imageURL: image
+        });
+        // }
+      });
+  });
 
   app.get("/rate", function(req, res) {
-    
-  })
+    res.render("rate");
+  });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
